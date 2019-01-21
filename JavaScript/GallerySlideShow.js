49711752371cc656666ca9ffa8c9b899c6cd29ae
polon59 <przemyslaw.polczak@gmail.com>
2019-01-21 23:09:41 +0100

    Change variables names in JS scripts

--- JavaScript/GallerySlideShow.js
+++ JavaScript/GallerySlideShow.js
@@ -6,12 +6,47 @@ class GallerySlideShow{
         this.currentCategory;
         this.currentCategoryIndex;
         this.currentIndex;
+        this.currentCategoryList = new Array();
+        this.currentCategoryImgList = new Array();
     }
 
 
     displayModal(imageBox){
-        console.log(imageBox.getAttribute('data-category'));
-        console.log(imageBox.getAttribute('data-index'));
+    //    let imageInModal = $("#modal").getElementsByTagName('img')[0];
 
+    // set current image in modal to index from categorylist
+       $("#modal").show();
+
+        // console.log(imageBox.getAttribute('data-category'));
+        // console.log(imageBox.getAttribute('data-index'));
+
+    }
+
+
+
+
+
+    changeCurrentCategoryImgList(newCategory){
+        if (newCategory == "all") {
+            this.currentCategoryList = document.getElementsByClassName("imageBox");
+        } else {
+            this.currentCategoryList = document.getElementsByClassName(newCategory);
+        }
+
+        console.log(`new category set to ${newCategory}`)
+
+
+        for (let divElement of this.currentCategoryList) {
+            this.currentCategoryImgList.push(divElement.getElementsByTagName('img')[0]);
+            console.log("adding image of specified category to list");
+        }
+
+        
+    }
+
+
+    changeCategory(newCategory){
+        this.changeCurrentCategoryImgList(newCategory);
+        this.currentCategory = newCategory;
     }
 }
--- JavaScript/imageFilter.js
+++ JavaScript/imageFilter.js
@@ -1,14 +1,18 @@
+var gallerySlides = new GallerySlideShow();
+
 function filterSeclection(categoryName) {
   if (categoryName == "all") {
     $(".image-box").show();
+    gallerySlides.changeCategory("all");
   }else{
     $(".image-box").hide();
     $(`.${categoryName}`).show();
+    gallerySlides.changeCategory(categoryName);
   }
 }
 
 
-var gallerySlides = new GallerySlideShow();
+
 
 function openModal(imageBox) {
   gallerySlides.displayModal(imageBox);
